452a620e450871562b6eeaaca6fe3454b605ee44
Illia Kaialainien <illia.kaialainien@gmail.com>
2023-12-06 16:37:18 +0200

    monitor app has been added

--- snort3_monitor/snort3_monitor/settings.py
+++ snort3_monitor/snort3_monitor/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "monitor.apps.MonitorConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
