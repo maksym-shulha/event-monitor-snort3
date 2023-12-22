c83d55ae3dcfd7c630ee1ab54985d086a5f54688
yura <komarnickijura@gmail.com>
2023-12-22 15:52:13 +0200

    Test with Event App

--- snort3_monitor/monitor/tests/test_api.py
+++ snort3_monitor/monitor/tests/test_api.py
@@ -29,7 +29,7 @@ class EventAPIListViewTest(APITestCase):
             dst_addr="10.0.0.1",
             dst_port=80,
         )
-        
+
         cls.event_2 = Event.objects.create(
             rule=cls.rule,
             timestamp=timestamp,
@@ -39,69 +39,68 @@ class EventAPIListViewTest(APITestCase):
             dst_addr="10.0.0.2",
             dst_port=80,
         )
-    
+
     def test_filter_src_addr(self):
         url = reverse('event-list-update')
         response = self.client.get(url, {'src_addr': '192.168.1.1'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, OrderedDict)
         self.assertTrue(response.data)
-        
+
         results = response.data.get('results', [])
         self.assertTrue(results)
         item = results[0] if results else None
         self.assertIn('192.168.1.1', item.get('src_addr', []))
         self.assertEqual(len(results), 1)
-     
+
     def test_filter_proto(self):
         url = reverse('event-list-update')
         response = self.client.get(url, {'proto': 'UDP'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, OrderedDict)
         self.assertTrue(response.data)
-        
+
         results = response.data.get('results', [])
         self.assertTrue(results)
         item = results[0] if results else None
         self.assertIn('UDP', item.get('proto', []))
         self.assertEqual(len(results), 1)
-          
+
     def test_filter_multiple_params(self):
         url = reverse('event-list-update')
         response = self.client.get(url, {'src_addr': '192.168.1.1', 'proto': 'TCP'})
-        
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, OrderedDict)
         self.assertIsNotNone(response.data)
 
         results = response.data.get('results', [])
         self.assertTrue(results)
-        
+
         item = results[0] if results else None
         self.assertEqual(item.get('src_addr'), '192.168.1.1')
         self.assertEqual(item.get('proto'), 'TCP')
         self.assertEqual(len(results), 1)
-        
+
     def test_filetr_multiple_different_params(self):
         url = reverse('event-list-update')
         response = self.client.get(url, {'dst_port': '80', 'dst_port': '80'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
         results = response.data.get('results', [])
         self.assertTrue(results)
         self.assertEqual(len(results), 2)
-        
         item1 = results[0] if results else None
         item2 = results[1] if len(results) > 1 else None
 
         self.assertEqual(item1.get('dst_port'), 80)
         self.assertEqual(item2.get('dst_port'), 80)
-    
+
     def test_invalid_params(self):
         url = reverse('event-list-update')
         response = self.client.get(url, {'addee': 'fo'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
+
     def test_patch_method_no_filtering(self):
         url = reverse('event-list-update')
         response = self.client.patch(url)
@@ -110,27 +109,14 @@ class EventAPIListViewTest(APITestCase):
         self.assertEqual(response.data, {"message": "All events are marked as deleted."})
 
         events = Event.objects.all()
-        self.assertEqual(events.count(), 2) 
+        self.assertEqual(events.count(), 2)
         for event in events:
             self.assertTrue(event.mark_as_deleted)
-    
+
     def test_order_by_id(self):
         url = reverse('event-list-update')
         response = self.client.get(url)
-        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         results = response.data.get('results', [])
 
-        self.assertEqual([result['id'] for result in results], sorted([1, 2])) 
-
-   
-    
-
- 
-
-
-        
-        
-    
-        
-  
+        self.assertEqual([result['id'] for result in results], sorted([1, 2]))
--- snort3_monitor/monitor/tests/test_serializer.py
+++ snort3_monitor/monitor/tests/test_serializer.py
@@ -10,12 +10,14 @@ class EventSerializerTest(TestCase):
 
     def setUp(self):
         self.rule = Rule.objects.create(
-            sid=1, gid=1, rev=1, 
-            action="Test Action", 
+            sid=1,
+            gid=1,
+            rev=1,
+            action="Test Action",
             message="Test Message",
             data_json={"key": "value"}
         )
-        
+
         self.event = Event.objects.create(
             rule=self.rule,
             timestamp=datetime.datetime.now(),
@@ -34,25 +36,25 @@ class EventSerializerTest(TestCase):
         self.assertEqual(serialized_data["timestamp"], self.event.timestamp.isoformat())
         self.assertEqual(serialized_data["src_addr"], self.event.src_addr)
         self.assertEqual(serialized_data["proto"], self.event.proto)
-    
+
     @unittest.expectedFailure
     def test_invalid_data_validation(self):
         invalid_data = {
-            'rule': 123,  
-            'timestamp': "invalid_format",  
-            'src_addr': "",  
+            'rule': 123,
+            'timestamp': "invalid_format",
+            'src_addr': "",
         }
-        
+
         serializer = EventSerializer(data=invalid_data)
         self.assertFalse(serializer.is_valid())
-        self.assertEqual(len(serializer.errors), 4)  
+        self.assertEqual(len(serializer.errors), 4)
         self.assertIn('rule', serializer.errors)
         self.assertIn('timestamp', serializer.errors)
         self.assertIn('src_addr', serializer.errors)
-    
+
     def test_dst_port(self):
         self.assertIsInstance(self.event.dst_port, int)
-    
+
     def test_invalid_dst_port_validation(self):
         invalid_data = {
            'dst_port': "invalid_port",
@@ -60,7 +62,7 @@ class EventSerializerTest(TestCase):
         serializer = EventSerializer(data=invalid_data)
         self.assertFalse(serializer.is_valid())
         self.assertIn('dst_port', serializer.errors)
-    
+
     def test_required_fields_existence(self):
         serializer = EventSerializer(data={
             'rule': self.rule.pk,
@@ -70,15 +72,15 @@ class EventSerializerTest(TestCase):
             'proto': self.event.proto,
         })
         self.assertTrue(serializer.is_valid(), serializer.errors)
-    
+
     def test_get_sid(self):
         serializer = EventSerializer(instance=self.event)
         self.assertEqual(serializer.get_sid(self.event), self.rule.sid)
-    
+
     def test_get_action(self):
         serializer = EventSerializer(instance=self.event)
         self.assertEqual(serializer.get_action(self.event), self.rule.action)
-        
+
     def test_get_message(self):
         serializer = EventSerializer(instance=self.event)
         self.assertEqual(serializer.get_message(self.event), self.rule.message)
