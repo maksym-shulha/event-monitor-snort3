4f21af217dfbd4bf949510bf5b86bf8a07ec28f6
Yura <komarnickijura@gmail.com>
2023-12-23 15:41:34 +0200

    Added test with count serializer sid and addr_pair

--- snort3_monitor/snort3_monitor/settings.py
+++ snort3_monitor/snort3_monitor/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
